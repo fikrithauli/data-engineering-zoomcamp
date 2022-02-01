2721022226f8fb1cdb1c2f5507b0fa209c6fa586
Fikri Thauli <fikrithauli@gmail.com>
2022-02-01 19:23:46 +0700

    add new dags data-ingestion-zones

---
+++ week_2_data_ingestion/airflow/dags_new/data_ingestion_zones.py
@@ -0,0 +1,93 @@
+import os
+import logging
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+from google.cloud import storage
+import pyarrow.csv as pv
+import pyarrow.parquet as pq
+
+PROJECT_ID = "dtc-de-bootcamp"
+BUCKET = "dtc_data_lake_dtc-de-bootcamp"
+
+dataset_file = "zone_lookup.csv"
+dataset_url = f"https://s3.amazonaws.com/nyc-tlc/misc/taxi+_{dataset_file}"
+path_to_local = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+parquet_file = dataset_file.replace(".csv", ".parquet")
+
+def format_to_parquet(src_file):
+    if not src_file.endswith('.csv'):
+        logging.error("Can only accept source files in CSV format, for the moment")
+        return
+    table = pv.read_csv(src_file)
+    pq.write_table(table, src_file.replace('.csv', '.parquet'))
+
+# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    :param bucket: GCS bucket name
+    :param object_name: target path & file-name
+    :param local_file: source path & file-name
+    :return:
+    """
+    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+    # End of Workaround
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+default_args = {
+    "owner": "airflow",
+    "start_date": days_ago(1),
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+with DAG(
+    dag_id = "data_ingestion_zones_lookup_gcs",
+    schedule_interval = "@once",
+    default_args = default_args,
+    catchup = False,
+    max_active_runs = 1,
+    tags = ['dtc-de: homework_week_2'],
+) as dag:
+
+    download_zones_data = BashOperator(
+        task_id = "download_zones_data",
+        bash_command = f"curl -sSLf {dataset_url} > {path_to_local}/{dataset_file}"
+    )
+
+    format_to_parquet_task = PythonOperator(
+        task_id = "format_to_parquet",
+        python_callable = format_to_parquet,
+        op_kwargs ={
+            "src_file": f"{path_to_local}/{dataset_file}"
+        }
+    )
+
+    upload_to_gcs_task = PythonOperator(
+        task_id = "upload_to_gcs",
+        python_callable = upload_to_gcs,
+        op_kwargs = {
+            "bucket": BUCKET,
+            "object_name": f"raw/{parquet_file}",
+            "local_file": f"{path_to_local}/{parquet_file}"
+        }
+    )
+
+    remove_file_task =  BashOperator(
+        task_id="remove_csv_file",
+        bash_command=f"rm {path_to_local}/{dataset_file} {path_to_local}/{parquet_file}"
+    )
+
+    download_zones_data >> format_to_parquet_task >> upload_to_gcs_task >> remove_file_task
