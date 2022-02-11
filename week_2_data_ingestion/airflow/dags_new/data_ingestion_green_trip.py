79d36d2a3d6903b7d1efb3d0a7a4dcd47381f9af
Fikri Thauli <fikrithauli@gmail.com>
2022-02-11 07:16:07 +0700

    add new dag for ingest green taxi data

---
+++ week_2_data_ingestion/airflow/dags_new/data_ingestion_green_trip.py
@@ -0,0 +1,100 @@
+import os
+import logging
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+from google.cloud import storage
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
+import pyarrow.csv as pv
+import pyarrow.parquet as pq
+
+
+PROJECT_ID = "dtc-de-bootcamp"
+BUCKET = "dtc_data_lake_dtc-de-bootcamp"
+
+dataset_file = "green_tripdata_{{ data_interval_start.strftime(\'%Y-%m\') }}.csv"
+dataset_url = f"https://s3.amazonaws.com/nyc-tlc/trip+data/{dataset_file}"
+path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+parquet_file = dataset_file.replace(".csv", ".parquet")
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", "trips_data_all")
+
+def format_to_parquet(src_file):
+    if not src_file.endswith('.csv'):
+        logging.error("Can only accept source files in CSV format, for the moment")
+        return
+    table = pv.read_csv(src_file)
+    pq.write_table(table, src_file.replace('.csv', '.parquet'))
+
+
+# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    :param bucket: GCS bucket name
+    :param object_name: target path & file-name
+    :param local_file: source path & file-name
+    :return:
+    """
+    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+    # End of Workaround
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2019,1,1),
+    "end_date": datetime(2020,12,30),
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+# NOTE: DAG declaration - using a Context Manager (an implicit way)
+with DAG(
+    dag_id="data_ingestion_gcs_dag_green",
+    schedule_interval="0 5 2 * *",
+    default_args=default_args,
+    catchup=True,
+    max_active_runs=3,
+    tags=['dtc-de: homework_week_2'],
+) as dag:
+
+    download_dataset_task = BashOperator(
+        task_id="download_dataset_task",
+        bash_command=f"curl -sSLf {dataset_url} > {path_to_local_home}/{dataset_file}"
+    )
+
+    format_to_parquet_task = PythonOperator(
+        task_id="format_to_parquet_task",
+        python_callable=format_to_parquet,
+        op_kwargs={
+            "src_file": f"{path_to_local_home}/{dataset_file}",
+        },
+    )
+
+    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
+    local_to_gcs_task = PythonOperator(
+        task_id="local_to_gcs_task",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw/{parquet_file}",
+            "local_file": f"{path_to_local_home}/{parquet_file}",
+        },
+    )
+
+    remove_file_task =  BashOperator(
+        task_id="remove_csv_file",
+        bash_command=f"rm {path_to_local_home}/{dataset_file}"
+    )
+
+    download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> remove_file_task
