55f495d6dfc0c143c61b67029baa86792bfd095f
Fikri Thauli <fikri.thauli@sigma.co.id>
2022-02-22 00:08:26 +0000

    fix count of record data

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -40,9 +40,9 @@ SELECT
 FROM tripdata
 WHERE rn = 1
 
--- dbt build --m <model.sql> --var 'is_test_run: false'
-{% if var('is_test_run', default=true) %}
+-- -- dbt build --m <model.sql> --var 'is_test_run: false'
+-- {% if var('is_test_run', default=true) %}
 
-  limit 100
+--   limit 100
 
-{% endif %}
+-- {% endif %}
