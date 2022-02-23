c9e1dc5ee2b5167a96abac0c4601c32b448c666c
Fikri Thauli <fikri.thauli@sigma.co.id>
2022-02-23 00:08:11 +0000

    add fhv_trip model

---
+++ models/core/fact_fhv_trips.sql
@@ -0,0 +1,29 @@
+{{ config(materialized='table') }}
+
+WITH fhv_data AS (
+    SELECT * 
+    FROM {{ ref('stg_fhv_tripdata') }}
+),
+
+dim_zones AS (
+    SELECT * 
+    FROM {{ ref('dim_zones') }}
+    WHERE Borough != 'Unknown'
+)
+
+SELECT 
+    fhv_data.dispatching_base_num,
+    fhv_data.pickup_datetime,
+    fhv_data.dropoff_datetime,
+    fhv_data.PULocationID,
+    pickup_zone.borough AS pickup_borough, 
+    pickup_zone.zone AS pickup_zone,
+    fhv_data.DOLocationID,
+    dropoff_zone.borough AS dropoff_borough, 
+    dropoff_zone.zone AS dropoff_zone,
+    fhv_data.SR_Flag
+FROM fhv_data
+INNER JOIN dim_zones AS pickup_zone
+ON fhv_data.PULocationID = pickup_zone.LocationID
+INNER JOIN dim_zones AS dropoff_zone
+ON fhv_data.DOLocationID = dropoff_zone.LocationID
---
+++ models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,25 @@
+{{ config(materialized='view') }}
+
+WITH tripdata AS (
+  SELECT *,
+        ROW_NUMBER() OVER(PARTITION BY dispatching_base_num, pickup_datetime) AS rn
+  FROM {{ source('staging', 'external_fhv_tripdata_partitioned_clustered') }}
+  WHERE dispatching_base_num IS NOT NULL
+)
+
+SELECT 
+    dispatching_base_num,
+    CAST(pickup_datetime AS TIMESTAMP) AS pickup_datetime,
+    CAST(dropoff_datetime AS TIMESTAMP) AS dropoff_datetime,
+    CAST(PULocationID AS INTEGER) AS PULocationID,
+    CAST(DOLocationID AS INTEGER) AS DOLocationID,
+    CAST(SR_Flag AS INTEGER) AS SR_Flag
+FROM tripdata
+WHERE rn = 1
+
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
